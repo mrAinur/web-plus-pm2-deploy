26740da211deab1e215881554b9899cd55fecd30
Ainur <mr.ainur.02@mail.ru>
2024-01-27 23:41:14 +0500

    Добавлен крашроут и описание ридми

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Деплой приложения на сервер с использованием pm2
 
 Стартеркит проекта по автоматизации деплоя фронтенда и бэкенда при помощи pm2 (pm2 deploy)
+
+## Информация для проверки
+
+1. IP адрес 158.160.126.133
+2. Frontend https://ainur.social.nomoredomainsmonster.ru/
+3. Backend https://api.ainur.social.nomoredomainsmonster.ru/
--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,15 +1,11 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import {
-  Request,
-  Response,
-  NextFunction,
-} from 'express';
-import User from '../models/user';
-import { JWT_SECRET } from '../config';
-import BadRequestError from '../errors/bad-request-error';
-import NotFoundError from '../errors/not-found-error';
-import ConflictError from '../errors/conflict-error';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import User from "../models/user";
+import { JWT_SECRET } from "../config";
+import BadRequestError from "../errors/bad-request-error";
+import NotFoundError from "../errors/not-found-error";
+import ConflictError from "../errors/conflict-error";
 
 const login = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
@@ -17,8 +13,7 @@ const login = (req: Request, res: Response, next: NextFunction) => {
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET);
       return res
-        .cookie('jwt', token, {
-
+        .cookie("jwt", token, {
           maxAge: 3600000,
           httpOnly: true,
           sameSite: true,
@@ -29,20 +24,25 @@ const login = (req: Request, res: Response, next: NextFunction) => {
 };
 
 const createUser = (req: Request, res: Response, next: NextFunction) => {
-  const {
-    name, about, avatar, password, email,
-  } = req.body;
+  const { name, about, avatar, password, email } = req.body;
 
-  bcrypt.hash(password, 10)
-    .then((hash) => User.create({
-      name, about, avatar, email, password: hash,
-    }))
+  bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      User.create({
+        name,
+        about,
+        avatar,
+        email,
+        password: hash,
+      })
+    )
     .then((data) => res.status(201).send(data))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === "ValidationError") {
         next(new BadRequestError(err.message));
       } else if (err.code === 11000) {
-        next(new ConflictError('Пользователь с данным email уже существует'));
+        next(new ConflictError("Пользователь с данным email уже существует"));
       } else {
         next(err);
       }
@@ -51,7 +51,9 @@ const createUser = (req: Request, res: Response, next: NextFunction) => {
 
 const getUserData = (id: string, res: Response, next: NextFunction) => {
   User.findById(id)
-    .orFail(() => new NotFoundError('Пользователь по заданному id отсутствует в базе'))
+    .orFail(
+      () => new NotFoundError("Пользователь по заданному id отсутствует в базе")
+    )
     .then((users) => res.send(users))
     .catch(next);
 };
@@ -65,24 +67,23 @@ const getCurrentUser = (req: Request, res: Response, next: NextFunction) => {
 };
 
 const updateUserData = (req: Request, res: Response, next: NextFunction) => {
-  const { user: { _id }, body } = req;
+  const {
+    user: { _id },
+    body,
+  } = req;
   User.findByIdAndUpdate(_id, body, { new: true, runValidators: true })
-    .orFail(() => new NotFoundError('Пользователь по заданному id отсутствует в базе'))
+    .orFail(
+      () => new NotFoundError("Пользователь по заданному id отсутствует в базе")
+    )
     .then((user) => res.send(user))
     .catch(next);
 };
 
-const updateUserInfo = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => updateUserData(req, res, next);
+const updateUserInfo = (req: Request, res: Response, next: NextFunction) =>
+  updateUserData(req, res, next);
 
-const updateUserAvatar = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => updateUserData(req, res, next);
+const updateUserAvatar = (req: Request, res: Response, next: NextFunction) =>
+  updateUserData(req, res, next);
 
 export {
   login,
